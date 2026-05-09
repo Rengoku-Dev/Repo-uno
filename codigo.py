f76a81fdf8c0c53f72a90ec81f7da7c5991f7ebf
Cristhian (Crispi) <og.timado18og.wisper@gmail.com>
2026-05-09 10:03:02 -0500

    subiendo el archivo de python

---
+++ codigo.py
@@ -0,0 +1,10 @@
+# Algoritmo para sumar 2 números distintos de 0
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+
+if num1 != 0 and num2 != 0:
+    suma = num1 + num2
+    print("La suma es:", suma)
+else:
+    print("Los números no deben ser 0")
